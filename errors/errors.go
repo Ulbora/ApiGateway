9609e8093b7b49d611b2dfca09d7cd8273b2edd8
ken <kenulborapublic@gmail.com>
2018-04-22 15:53:18 -0400

    updates

--- errors/errors.go
+++ errors/errors.go
@@ -1,1 +1,49 @@
 package errors
+
+import (
+	cm "ApiGateway/common"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+//GatewayErrors gateway errors
+type GatewayErrors struct {
+	ClientID int64
+	Host     string
+}
+
+//ErrorLog ErrorLog
+type ErrorLog struct {
+	ClientID   int64  `json:"clientId"`
+	ErrCode    int    `json:"errorCode"`
+	Message    string `json:"message"`
+	RouteID    int64  `json:"routeId"`
+	RouteURIID int64  `json:"routeUriId"`
+}
+
+//GatewayErrorResponse GatewayErrorResponse
+type GatewayErrorResponse struct {
+	Success bool `json:"success"`
+}
+
+//SaveErrors SaveErrors
+func (e *GatewayErrors) SaveErrors(obj interface{}) (*GatewayErrorResponse, int) {
+	var code int
+	var rtn GatewayErrorResponse
+	var errURL = e.Host + "/rs/cluster/route/error"
+	fmt.Print("errURL: ")
+	fmt.Println(errURL)
+	j := cm.GetJSONEncode(obj)
+	req, fail := cm.GetRequest(errURL, http.MethodPost, j)
+	if !fail {
+		cid := strconv.FormatInt(e.ClientID, 10)
+		req.Header.Set("u-client-id", cid)
+		req.Header.Set("Content-Type", "application/json")
+		code = cm.ProcessServiceCall(req, &rtn)
+	} else {
+		fmt.Println("save error failed")
+		code = http.StatusBadRequest
+	}
+	return &rtn, code
+}
---
+++ errors/errors_test.go
@@ -0,0 +1,46 @@
+package errors
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGatewayErrors_SaveErrorsUrl(t *testing.T) {
+	var e GatewayErrors
+	e.ClientID = 403
+	e.Host = "://localhost:3011"
+	var el ErrorLog
+	el.ClientID = e.ClientID
+	el.ErrCode = 400
+	el.RouteID = 22
+	el.RouteURIID = 33
+	el.Message = "error from cluster gateway api"
+	resp, code := e.SaveErrors(el)
+	fmt.Print("resp in clear cluster: ")
+	fmt.Println(resp)
+	fmt.Print("code in clear cluster: ")
+	fmt.Println(code)
+	if code != 400 {
+		t.Fail()
+	}
+}
+
+func TestGatewayErrors_SaveErrors(t *testing.T) {
+	var e GatewayErrors
+	e.ClientID = 403
+	e.Host = "http://localhost:3011"
+	var el ErrorLog
+	el.ClientID = e.ClientID
+	el.ErrCode = 400
+	el.RouteID = 22
+	el.RouteURIID = 33
+	el.Message = "error from cluster gateway api"
+	resp, code := e.SaveErrors(el)
+	fmt.Print("resp in clear cluster: ")
+	fmt.Println(resp)
+	fmt.Print("code in clear cluster: ")
+	fmt.Println(code)
+	if code != 200 || !resp.Success {
+		t.Fail()
+	}
+}
