cc0e5d27ee575d8d8cbbc06d00e3fd562ce6594f
ken <kenulborapublic@gmail.com>
2018-04-28 19:59:47 -0400

    updates

--- managers/gatewayManager.go
+++ managers/gatewayManager.go
@@ -2,9 +2,11 @@ package managers
 
 import (
 	"fmt"
-	"strconv"
+	"os"
+	//"strconv"
 	//cl "ApiGateway/cluster"
 	ch "ApiGateway/cache"
+	cl "ApiGateway/cluster"
 )
 
 //GatewayRoutes gateway routes
@@ -12,6 +14,7 @@ type GatewayRoutes struct {
 	Route    string
 	APIKey   string
 	ClientID int64
+	Cache    ch.RouteCache
 }
 
 //GatewayRouteURL url
@@ -27,17 +30,72 @@ type GatewayRouteURL struct {
 }
 
 //GetGatewayRoute GetGatewayRoute
-func (gw *GatewayRoutes) GetGatewayRoute(getActive bool, routeName string) *GatewayRouteURL {
+func (gw *GatewayRoutes) GetGatewayRoute(getActive bool, route string, routeName string) *GatewayRouteURL {
 	var rtnVal GatewayRouteURL
-	var c ch.RouteCache
-	c.ClientID = strconv.FormatInt(gw.ClientID, 10)
-	crts := c.GetRoutes(routeName)
+	var rtn *[]cl.GatewayClusterRouteURL
+	crts := gw.Cache.GetRoutes(route)
 	fmt.Print("crts: ")
 	fmt.Println(crts)
 	if crts != nil {
 		// work with cached routes and the delete
+		rtn = crts
+		go func(gwr *GatewayRoutes, rt string) {
+			gwr.readAndStore(rt)
+		}(gw, route)
 	} else {
-		//get routes from server and then cache
+		rtn = gw.readAndStore(route)		
+		//fmt.Print("code: ")
+		//fmt.Println(code)
+	}
+	if len(*rtn) > 0 && getActive == true {
+		for r := range *rtn {
+			if (*rtn)[r].Active == true {
+				rtnVal = parseGatewayRoutes((*rtn)[r])
+				break
+			}
+		}
+	} else if len(*rtn) > 0 {
+		for r := range *rtn {
+			if (*rtn)[r].Name == routeName {
+				rtnVal = parseGatewayRoutes((*rtn)[r])
+				break
+			}
+		}
 	}
 	return &rtnVal
+}
+
+func getAPIGatewayURL() string {
+	var rtn = ""
+	if os.Getenv("API_GATEWAY") != "" {
+		rtn = os.Getenv("API_GATEWAY")
+	} else {
+		rtn = "http://localhost:3011"
+	}
+	return rtn
+}
+
+func parseGatewayRoutes(rt cl.GatewayClusterRouteURL) GatewayRouteURL {
+	var rtn GatewayRouteURL
+	rtn.Active = rt.Active
+	rtn.CircuitOpen = rt.CircuitOpen
+	rtn.Name = rt.Name
+	rtn.OpenFailCode = rt.OpenFailCode
+	rtn.Route = rt.Route
+	rtn.RouteID = rt.RouteID
+	rtn.URL = rt.URL
+	rtn.URLID = rt.URLID
+	return rtn
+}
+
+func (gw *GatewayRoutes) readAndStore(route string) *[]cl.GatewayClusterRouteURL {
+	var clst cl.GatewayRoutes
+	clst.ClientID = gw.ClientID
+	clst.APIKey = gw.APIKey
+	clst.Host = getAPIGatewayURL()
+	rtn, _ := clst.GetClusterGwRoutes(route)
+	fmt.Print("resp: ")
+	fmt.Println(rtn)
+	gw.Cache.SaveRoutes(route, rtn)
+	return rtn
 }
--- managers/gatewayManager_test.go
+++ managers/gatewayManager_test.go
@@ -1,15 +1,97 @@
 package managers
 
 import (
+	ch "ApiGateway/cache"
+	cl "ApiGateway/cluster"
 	"fmt"
+	"os"
+	"strconv"
 	"testing"
+	"time"
 )
 
+var c ch.RouteCache
+
 func TestGatewayRoutes_GetGatewayRoutes(t *testing.T) {
+	//var c ch.RouteCache
 	var gw GatewayRoutes
 	gw.ClientID = 403
 	gw.APIKey = "403"
-	chrt := gw.GetGatewayRoute(true, "challenge")
+	c.ClientID = strconv.FormatInt(gw.ClientID, 10)
+	gw.Cache = c
+	chrt := gw.GetGatewayRoute(true, "challenge", "")
 	fmt.Print("route: ")
 	fmt.Println(chrt)
+	if !chrt.Active {
+		t.Fail()
+	}
+}
+
+func TestGatewayRoutes_GetGatewayRoutesNotActive(t *testing.T) {
+	var gw GatewayRoutes
+	gw.ClientID = 403
+	gw.APIKey = "403"
+	c.ClientID = strconv.FormatInt(gw.ClientID, 10)
+	gw.Cache = c
+	chrt := gw.GetGatewayRoute(false, "challenge", "blue")
+	fmt.Print("route: ")
+	fmt.Println(chrt)
+	time.Sleep(time.Second * 2)
+	cr := c.GetRoutes("challenge")
+	fmt.Print("cashed routes after read: ")
+	fmt.Println(cr)
+	if len(*cr) == 0 {
+		t.Fail()
+	}
+	if chrt.Active {
+		t.Fail()
+	}
+}
+
+func TestGatewayRoutes_readAndStore(t *testing.T) {
+	var gw GatewayRoutes
+	gw.ClientID = 403
+	gw.APIKey = "403"
+	c.ClientID = strconv.FormatInt(gw.ClientID, 10)
+	gw.Cache = c
+	rts := gw.readAndStore("challenge")
+	fmt.Print("read routes: ")
+	fmt.Println(rts)
+	if len(*rts) == 0 {
+		t.Fail()
+	}
+}
+
+func Test_getAPIGatewayURL(t *testing.T) {
+	h := getAPIGatewayURL()
+	if h != "http://localhost:3011" {
+		t.Fail()
+	}
+}
+
+func Test_getAPIGatewayURLEnv(t *testing.T) {
+	os.Setenv("API_GATEWAY", "test")
+	h := getAPIGatewayURL()
+	if h != "test" {
+		t.Fail()
+	}
+}
+
+func Test_parseGatewayRoutes(t *testing.T) {
+	//var rts = make([]cl.GatewayClusterRouteURL, 0)
+	var r1 cl.GatewayClusterRouteURL
+	r1.Active = true
+	r1.CircuitOpen = false
+	r1.Name = "challenge"
+	r1.OpenFailCode = 401
+	r1.Route = "challenge"
+	r1.RouteID = 1
+	r1.URL = "test1"
+	r1.URLID = 1
+	res := parseGatewayRoutes(r1)
+	if res.Active != true || res.CircuitOpen != false || res.Name != "challenge" || res.OpenFailCode != 401 ||
+		res.Route != "challenge" || res.RouteID != 1 || res.URL != "test1" || res.URLID != 1 {
+		t.Fail()
+	}
+
 }
