18cbae152532928133879a1ace29e3b251fae2e1
ken <kenulborapublic@gmail.com>
2018-05-08 21:28:01 -0400

    updates

--- main.go
+++ main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	ch "ApiGateway/cache"
 	hd "ApiGateway/handlers"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"os"
+	"strconv"
 )
 
+var h hd.Handler
+var c ch.RouteCache
+
 func main() {
+	var cid string
+	var key string
+	cid = os.Args[1]
+	key = os.Args[2]
+	//fmt.Print("cid: ")
+	//fmt.Println(cid)
+
+	//fmt.Print("key: ")
+	//fmt.Println(key)
+
+	h.Cache = c
+	if cid == "" {
+		cid = getClientID()
+	}
+
+	if key == "" {
+		key = getAPIKey()
+	}
+	h.ClientID, _ = strconv.ParseInt(cid, 10, 0)
+	h.APIKey = key
+
+	//fmt.Print("cid: ")
+	//fmt.Println(h.ClientID)
+
+	//fmt.Print("key: ")
+	//fmt.Println(h.APIKey)
+
 	fmt.Println("Api Gateway running inside network on port 3020!")
 	router := mux.NewRouter()
-	var h hd.Handler
 	//gateway routes
 	router.HandleFunc("/np/{route}/{rname}/{fpath:[^.]+}", h.HandleGwRoute)
 	router.HandleFunc("/{route}/{fpath:[^ ]+}", h.HandleGwRoute)
@@ -18,4 +50,12 @@ func main() {
 	router.HandleFunc("/{route}", h.HandleGwRoute)
 	http.ListenAndServe(":3020", router)
 
+}
+
+func getClientID() string {
+	return os.Getenv("API_GATEWAY_CLIENT_ID")
+}
+
+func getAPIKey() string {
+	return os.Getenv("API_GATEWAY_API_KEY")
 }
